7aba710e9accb62d7f195105d5bdf432bba6e637
Oshan <git config --global user.email>
2026-02-21 17:46:27 +0530

    add all

--- src/main/java/lk/ijse/mlsupermarket/bo/custom/CustomerBO.java
+++ src/main/java/lk/ijse/mlsupermarket/bo/custom/CustomerBO.java
@@ -1,4 +1,19 @@
 package lk.ijse.mlsupermarket.bo.custom;
 
-public interface CustomerBO {
+import lk.ijse.mlsupermarket.bo.SuperBO;
+import lk.ijse.mlsupermarket.dao.SuperDAO;
+import lk.ijse.mlsupermarket.entity.Customer;
+
+import java.util.ArrayList;
+
+public interface CustomerBO  extends SuperBO {
+
+
+    String generateId() throws Exception;
+    boolean save(Customer customer) throws Exception;
+    Customer search(String id) throws Exception;
+    boolean update(Customer customer) throws Exception;
+    boolean delete(String id) throws Exception;
+    ArrayList<Customer> getAll() throws Exception;
+
 }
--- src/main/java/lk/ijse/mlsupermarket/bo/impl/CustomerBOImpl.java
+++ src/main/java/lk/ijse/mlsupermarket/bo/impl/CustomerBOImpl.java
@@ -1,4 +1,44 @@
 package lk.ijse.mlsupermarket.bo.impl;
 
-public class CustomerBOImpl {
+import lk.ijse.mlsupermarket.bo.custom.CustomerBO;
+import lk.ijse.mlsupermarket.dao.DAOFactory;
+import lk.ijse.mlsupermarket.dao.custom.CustomerDAO;
+import lk.ijse.mlsupermarket.entity.Customer;
+
+import java.util.ArrayList;
+
+public class CustomerBOImpl implements CustomerBO {
+
+    CustomerDAO customerDAO = (CustomerDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOType.CUSTOMER);
+
+
+    @Override
+    public String generateId() throws Exception {
+        return customerDAO.generateId();
+    }
+
+    @Override
+    public ArrayList<Customer> getAll() throws Exception {
+        return customerDAO.getAll();
+    }
+
+    @Override
+    public boolean save(Customer customer) throws Exception {
+        return customerDAO.save(customer);
+    }
+
+    @Override
+    public boolean update(Customer customer) throws Exception {
+        return customerDAO.update(customer);
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        return customerDAO.delete(id);
+    }
+
+    @Override
+    public Customer search(String id) throws Exception {
+        return customerDAO.search(id);
+    }
 }
